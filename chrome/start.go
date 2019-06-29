58781e6f17d1ac6e9ea8a63434035e7108351493
zaddone <zaddone@qq.com>
2019-06-29 22:55:45 +0800

    up

--- chrome/start.go
+++ chrome/start.go
@@ -7,15 +7,20 @@ import(
 	"io"
 	"bytes"
 	"strings"
-	//"net/http"
+	"regexp"
 	"time"
 	"log"
+	"html"
 	"encoding/json"
+	"github.com/lunny/html2md"
 	"github.com/zaddone/studySystem/request"
 	"github.com/gorilla/websocket"
+	"github.com/zaddone/studySystem/config"
 )
 var (
 	port = "9222"
+
+	rooturl string = "https://www.toutiao.com"
 	op =[]string{
 		"--remote-debugging-port="+port,
 		"--headless",
@@ -26,15 +31,27 @@ var (
 	}
 	Ourl = "http://127.0.0.1:"+port
 	k = []byte{10,68,101,118,84,111,111,108,115,32}
+	retitle *regexp.Regexp
+	rec *regexp.Regexp
 
 )
 func init(){
+	var err error
+	retitle, err = regexp.Compile(`title: \'[\s\s]+?\'`)
+	if err != nil {
+		panic(err)
+	}
+	rec, err = regexp.Compile(`content: \'[\s\s]+?\'`)
+	if err != nil {
+		panic(err)
+	}
 	start(func(in string){
 		fmt.Println(in)
 		open("https://www.toutiao.com/ch/news_baby/",func(v interface{})error{
 			//fmt.Println(v)
 			requestId:=""
 			step:=0
+			id:=float64(time.Now().Unix())
 			runStream((v.(map[string]interface{}))["webSocketDebuggerUrl"].(string),func(_v interface{},c *websocket.Conn){
 				__v :=_v.(map[string]interface{})
 				if step == 0{
@@ -65,9 +82,10 @@ func init(){
 					//if err != nil {
 					//	log.Fatal("w:", err)
 					//}
+
 					err := c.WriteJSON(map[string]interface{}{
 						"method":"Network.getResponseBody",
-						"id":time.Now().Unix(),
+						"id":id,
 						"params":map[string]interface{}{"requestId":requestId},
 					})
 					if err != nil {
@@ -80,11 +98,31 @@ func init(){
 					//	return
 					//}
 					//fmt.Println(__v)
-					if strings.Contains(fmt.Sprintln(__v),"body"){
-						fmt.Println(__v)
-						panic(0)
-
+					//if strings.Contains(fmt.Sprintln(__v),"body"){
+					//	fmt.Println(__v)
+					//}
+					//return
+					_id := __v["id"]
+					if _id==nil {
+						return
 					}
+					if id != __v["id"].(float64){
+						return
+					}
+					//fmt.Println(__v)
+					body := __v["result"].(map[string]interface{})["body"].(string)
+					body_:= map[string]interface{}{}
+					json.Unmarshal([]byte(body),&body_)
+					for _,d := range body_["data"].([]interface{}){
+						if err := extract(rooturl + d.(map[string]interface{})["source_url"].(string)); err != nil {
+							fmt.Println(err)
+						}
+					}
+					err := c.WriteJSON(map[string]interface{}{"method":"Network.disable","id":99})
+					if err != nil {
+						log.Fatal("w:", err)
+					}
+
 
 				}
 
@@ -179,5 +217,52 @@ func runStream(u string,hand func(interface{},*websocket.Conn)){
 			//fmt.Println(db)
 		}
 	}()
+}
+
+func extract(uri string) error {
+	fmt.Println(uri)
+	return request.ClientHttp_(uri,"GET",nil,config.Conf.Header,
+	func(body io.Reader,st int)error {
+		db,err := ioutil.ReadAll(body)
+		if err != nil {
+			return err
+		}
+		if st != 200 {
+			return fmt.Errorf("%d %s",st,db)
+		}
+		//fmt.Println(string(db))
+		_extract(db)
+		//fmt.Println(string(db))
+		return nil
+	})
+
+}
+func _extract(body []byte) {
+	//retitle, err := regexp.Compile("title: \\'[\\s\\s]+?\\'")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//rec, err := regexp.Compile("content: \\'[\\s\\s]+?\\'")
+	//if err != nil {
+	//	panic(err)
+	//}
+	loc := retitle.FindIndex(body)
+	fmt.Println(loc)
+	if len(loc)==0 {
+		//fmt.println(string(body))
+		return
+	}
+	title := string(body[loc[0]+8:loc[1]-1])
+	fmt.Println(title)
+	loc_ := rec.FindIndex(body)
+	if len(loc_)==0 {
+		//fmt.println(title)
+		fmt.Println(string(body))
+		return
+	}
+	content:=html2md.Convert(html.UnescapeString(string(body[loc_[0]+10:loc_[1]-1])))
+	fmt.Println(content)
+	//fmt.println(string(db[loc[0]+8:loc[1]-1]))
+
 }
 
